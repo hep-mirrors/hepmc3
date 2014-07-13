c5f5fcf0172eb5ea2849d79353b346d7067b5b41
Andy Buckley <andy.buckley@cern.ch>
2014-07-13 12:10:44 +0100

    Adding HepMC.h and Version.h headers.

---
+++ include/HepMC3/HepMC.h
@@ -0,0 +1,14 @@
+// -*- C++ -*-
+//
+// This file is part of HepMC
+// Copyright (C) 2014 The HepMC collaboration (see AUTHORS for details)
+//
+#ifndef HEPMC_HEPMC_H
+#define HEPMC_HEPMC_H
+
+#include "HepMC3/Version.h"
+#include "HepMC3/GenEvent.h"
+#include "HepMC3/GenParticle.h"
+#include "HepMC3/GenVertex.h"
+
+#endif
---
+++ include/HepMC3/Version.h
@@ -0,0 +1,34 @@
+// -*- C++ -*-
+//
+// This file is part of HepMC
+// Copyright (C) 2014 The HepMC collaboration (see AUTHORS for details)
+//
+#ifndef HEPMC_VERSION_H
+#define HEPMC_VERSION_H
+
+#include <string>
+
+/// HepMC version string
+/// @todo Insert value from build system
+#define HEPMC_VERSION "3.0.0"
+
+/// HepMC version as an int (MICRO + 1000*MINOR + 1000000*MAJOR)
+// Use like "#if defined HEPMC_VERSION_CODE && LHAPDF_VERSION_CODE < 3001042" for < 3.1.42
+/// @todo Insert value from build system
+#define HEPMC_VERSION_CODE 3000000
+
+
+/// @todo Add feature detection macros (clearly version-related, and why proliferate headers?)
+
+
+namespace HepMC3 {
+
+
+  /// Get the HepMC library version string
+  inline std::string version() {
+    return HEPMC_VERSION;
+  }
+
+
+}
+#endif
