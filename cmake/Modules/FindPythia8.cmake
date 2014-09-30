65599a1e0289fd0b7eb163759a3fdcd197c77c8e
Witold Pokorski <witoldp@Witolds-MacBook-Pro.local>
2014-09-30 15:38:52 +0200

    improving FindPythia8

--- cmake/Modules/FindPythia8.cmake
+++ cmake/Modules/FindPythia8.cmake
@@ -1,23 +1,34 @@
-# Locate Pythia8 library
-# in a directory defined via PYTHIA8_ROOT_DIR environment variable
-#
+# - Locate pythia6 library
 # Defines:
+#
 #  PYTHIA8_FOUND
-#  PYTHIA8_LIBRARIES
+#  PYTHIA8_VERSION
+#  PYTHIA8_INCLUDE_DIR
+#  PYTHIA8_INCLUDE_DIRS (not cached)
+#  PYTHIA8_LIBRARY
+#  PYTHIA8_hepmcinterface_LIBRARY
+#  PYTHIA8_lhapdfdummy_LIBRARY
+#  PYTHIA8_LIBRARIES (not cached) : includes 3 libraries above; not to be used if lhapdf is used
 
-find_path(PYTHIA8_INCLUDE_DIR Pythia8/Pythia.h
-          HINTS $ENV{PYTHIA8_ROOT_DIR}/include ${PYTHIA8_ROOT_DIR}/include
-          $ENV{PYTHIA8_DIR}/include ${PYTHIA8_DIR}/include)
+find_path(PYTHIA8_INCLUDE_DIR Pythia.h Pythia8/Pythia.h
+          HINTS $ENV{PYTHIA8_ROOT_DIR}/include ${PYTHIA8_ROOT_DIR}/include)
 
-find_library(PYTHIA8_LIBRARIES NAMES pythia8
-             HINTS $ENV{PYTHIA8_ROOT_DIR}/lib ${PYTHIA8_ROOT_DIR}/lib
-             HINTS $ENV{PYTHIA8_DIR}/lib ${PYTHIA8_DIR}/lib)
+file(READ ${PYTHIA8_INCLUDE_DIR}/../xmldoc/Version.xml versionstr)
+string(REGEX REPLACE ".*Pythia:versionNumber.*default.*[0-9][.]([0-9]+).*" "\\1" PYTHIA8_VERSION "${versionstr}")
 
-message(STATUS "Pythia8 include directory: ${PYTHIA8_INCLUDE_DIR}")
+find_library(PYTHIA8_LIBRARY NAMES pythia8 Pythia8
+             HINTS $ENV{PYTHIA8_ROOT_DIR}/lib ${PYTHIA8_ROOT_DIR}/lib)
+
+find_library(PYTHIA8_lhapdfdummy_LIBRARY NAMES lhapdfdummy
+             HINTS $ENV{PYTHIA8_ROOT_DIR}/lib ${PYTHIA8_ROOT_DIR}/lib)
+
+set(PYTHIA8_INCLUDE_DIRS ${PYTHIA8_INCLUDE_DIR} ${PYTHIA8_INCLUDE_DIR}/Pythia8 )
+set(PYTHIA8_LIBRARIES ${PYTHIA8_LIBRARY} ${PYTHIA8_lhapdfdummy_LIBRARY})
 
 # handle the QUIETLY and REQUIRED arguments and set PYTHIA8_FOUND to TRUE if
 # all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pythia8 DEFAULT_MSG PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARIES)
 
-mark_as_advanced(PYTHIA8_FOUND PYTHIA8_LIBRARIES)
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pythia8 DEFAULT_MSG PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY PYTHIA8_lhapdfdummy_LIBRARY)
+
+mark_as_advanced(PYTHIA8_FOUND PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY PYTHIA8_lhapdfdummy_LIBRARY PYTHIA8_VERSION_CHECK)
