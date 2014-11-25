bec0bba5d5b949dab2761e554926a49dbdb0e52f
Andy Buckley <andy.buckley@cern.ch>
2014-11-25 13:55:17 +0000

    Using HEPMC_NO_DEPRECATED to remove deprecated functionality from the build

--- include/HepMC/GenVertex.h
+++ include/HepMC/GenVertex.h
@@ -3,16 +3,12 @@
 // This file is part of HepMC
 // Copyright (C) 2014 The HepMC collaboration (see AUTHORS for details)
 //
-#ifndef  HEPMC_GENVERTEX_H
-#define  HEPMC_GENVERTEX_H
-/**
- *  @file GenVertex.h
- *  @brief Definition of \b class GenVertex
- *
- *  @class HepMC::GenVertex
- *  @brief Stores vertex-related information
- *
- */
+/// @file GenVertex.h
+/// @brief Definition of \b class GenVertex
+//
+#ifndef HEPMC_GENVERTEX_H
+#define HEPMC_GENVERTEX_H
+
 #include "HepMC/Data/SmartPointer.h"
 #include "HepMC/Data/GenVertexData.h"
 #include "HepMC/FourVector.h"
@@ -23,11 +19,17 @@ using std::vector;
 
 namespace HepMC {
 
+
 /** @brief Type of iteration. Ssed by backward-compatibility interface */
+#ifndef HEPMC_NO_DEPRECATED
 enum IteratorRange { parents, children, family, ancestors, descendants, relatives };
+#endif
+
 
 class GenEvent;
 
+
+/// Stores vertex-related information
 class GenVertex {
 
 friend class GenEvent;
@@ -56,6 +58,8 @@ public:
 protected:
     /** @brief Print information about the vertex in event-listing format */
     void print_event_listing( std::ostream& ostr = std::cout ) const;
+
+
 //
 // Accessors
 //
@@ -86,51 +90,77 @@ public:
      */
     int barcode() const { return m_id; }
 
-//
-// Deprecated functionality
-//
-public:
-    HEPMC_DEPRECATED("Use GenParticlePtr instead of GenParticle*") void add_particle_in ( GenParticle *p ) { add_particle_in (GenParticlePtr(p)); } //!< Add incoming particle by raw pointer @deprecated Use GenVertex::add_particle_in( const GenParticlePtr &p ) instead
-    HEPMC_DEPRECATED("Use GenParticlePtr instead of GenParticle*") void add_particle_out( GenParticle *p ) { add_particle_out(GenParticlePtr(p)); } //!< Add outgoing particle by raw pointer @deprecated Use GenVertex::add_particle_out( const GenParticlePtr &p ) instead
 
+    /// @name Deprecated functionality
+    //@{
+
+    #ifndef HEPMC_NO_DEPRECATED
+
+    /// Add incoming particle by raw pointer
+    /// @deprecated Use GenVertex::add_particle_in( const GenParticlePtr &p ) instead
+    HEPMC_DEPRECATED("Use GenParticlePtr instead of GenParticle*")
+    void add_particle_in ( GenParticle *p ) { add_particle_in (GenParticlePtr(p)); }
+
+    /// Add outgoing particle by raw pointer
+    /// @deprecated Use GenVertex::add_particle_out( const GenParticlePtr &p ) instead
+    HEPMC_DEPRECATED("Use GenParticlePtr instead of GenParticle*")
+    void add_particle_out( GenParticle *p ) { add_particle_out(GenParticlePtr(p)); }
+
+    /// Define iterator by typedef
     typedef std::vector<GenParticlePtr>::const_iterator particles_in_const_iterator;
+    /// Define iterator by typedef
     typedef std::vector<GenParticlePtr>::const_iterator particles_out_const_iterator;
+    /// Define iterator by typedef
     typedef std::vector<GenParticlePtr>::iterator       particle_iterator;
 
-    HEPMC_DEPRECATED("Iterate over std container particles_in() instead") particles_in_const_iterator  particles_in_const_begin()  const { return m_particles_in.begin();  } //!< @deprecated Backward compatibility iterators
-    HEPMC_DEPRECATED("Iterate over std container particles_in() instead") particles_in_const_iterator  particles_in_const_end()    const { return m_particles_in.end();    } //!< @deprecated Backward compatibility iterators
-    HEPMC_DEPRECATED("Iterate over std container particles_out() instead") particles_out_const_iterator particles_out_const_begin() const { return m_particles_out.begin(); } //!< @deprecated Backward compatibility iterators
-    HEPMC_DEPRECATED("Iterate over std container particles_out() instead") particles_out_const_iterator particles_out_const_end()   const { return m_particles_out.end();   } //!< @deprecated Backward compatibility iterators
+    /// @deprecated Backward compatibility iterators
+    HEPMC_DEPRECATED("Iterate over std container particles_in() instead")
+    particles_in_const_iterator  particles_in_const_begin()  const { return m_particles_in.begin();  } //!< @deprecated Backward compatibility iterators
 
-    /** @brief @deprecated Backward compatibility iterators */
-    HEPMC_DEPRECATED() particle_iterator particles_begin(IteratorRange range) {
-        if( range == parents  ) return m_particles_in.begin();
-        if( range == children ) return m_particles_out.begin();
+    /// @deprecated Backward compatibility iterators
+    HEPMC_DEPRECATED("Iterate over std container particles_in() instead")
+    particles_in_const_iterator  particles_in_const_end()    const { return m_particles_in.end();    } //!< @deprecated Backward compatibility iterators
 
-        ERROR("GenVertex::particles_begin: Only 'parents' and 'children' ranges allowed.")
-        exit(-1);
+    /// @deprecated Backward compatibility iterators
+    HEPMC_DEPRECATED("Iterate over std container particles_out() instead")
+    particles_out_const_iterator particles_out_const_begin() const { return m_particles_out.begin(); } //!< @deprecated Backward compatibility iterators
 
-        return m_particles_in.begin();
+    /// @deprecated Backward compatibility iterators
+    HEPMC_DEPRECATED("Iterate over std container particles_out() instead")
+    particles_out_const_iterator particles_out_const_end()   const { return m_particles_out.end();   }
+
+    /// @deprecated Backward compatibility iterators
+    HEPMC_DEPRECATED("Use particles_in/out() functions instead")
+    particle_iterator particles_begin(IteratorRange range) {
+        if (range == parents) return m_particles_in.begin();
+        if (range == children) return m_particles_out.begin();
+        throw Exception("GenVertex::particles_begin: Only 'parents' and 'children' ranges allowed.");
     }
 
-    /** @brief //!< @deprecated Backward compatibility iterators */
+    /// @deprecated Backward compatibility iterators
     HEPMC_DEPRECATED() particle_iterator particles_end(IteratorRange range) {
-        if( range == parents  ) return m_particles_in.end();
-        if( range == children ) return m_particles_out.end();
-
-        ERROR("GenVertex::particles_begin: Only 'parents' and 'children' ranges allowed.")
-        exit(-1);
-
-        return m_particles_in.end();
+        if (range == parents) return m_particles_in.end();
+        if (range == children) return m_particles_out.end();
+        throw Exception("GenVertex::particles_begin: Only 'parents' and 'children' ranges allowed.");
     }
 
-    HEPMC_DEPRECATED("Use particles_in().size() instead")  int particles_in_size()  const { return m_particles_in.size();  } //!< @deprecated Backward compatibility
-    HEPMC_DEPRECATED("Use particles_out().size() instead") int particles_out_size() const { return m_particles_out.size(); } //!< @deprecated Backward compatibility
+    /// @deprecated Backward compatibility
+    HEPMC_DEPRECATED("Use particles_in().size() instead")
+    int particles_in_size()  const { return m_particles_in.size(); }
 
-//
-// Fields
-//
+    /// @deprecated Backward compatibility
+    HEPMC_DEPRECATED("Use particles_out().size() instead")
+    int particles_out_size() const { return m_particles_out.size(); }
+
+    #endif
+
+    //@}
+
+
 private:
+
+    /// @name Fields
+    //@{
     GenEvent      *m_event; //!< Parent event
     int            m_id;    //!< Vertex id
     GenVertexData  m_data;  //!< Vertex data
@@ -138,7 +168,10 @@ private:
     vector<GenParticlePtr>  m_particles_in;  //!< Incoming particle list
     vector<GenParticlePtr>  m_particles_out; //!< Outgoing particle list
     weak_ptr<GenVertex>     m_this;          //!< Pointer to shared pointer managing this vertex
+    //@}
+
 };
+
 
 } // namespace HepMC
 
--- include/HepMC/Setup.h
+++ include/HepMC/Setup.h
@@ -3,110 +3,128 @@
 // This file is part of HepMC
 // Copyright (C) 2014 The HepMC collaboration (see AUTHORS for details)
 //
-#ifndef  HEPMC_SETUP_H
-#define  HEPMC_SETUP_H
-/**
- *  @file Setup.h
- *  @brief Definition of \b class Setup
- *
- *  @class HepMC::Setup
- *  @brief Configuration for HepMC
- *
- *  Contains macro definitions for printing debug output, feature deprecation, etc.
- *  Static class - configuration is shared among all HepMC events
- *  and program threads
- *
- */
+/// @file Setup.h
+/// @brief Definition of \b class Setup
+
+#ifndef HEPMC_SETUP_H
+#define HEPMC_SETUP_H
+
 #include <iostream>
+#include <stdexcept>
 
 namespace HepMC {
 
 
-  class Setup {
-
-      /// Private constructor
-      Setup() {}
-      /// Private destructor
-      ~Setup() {}
+    /// @todo Move some of the helper functions, logging, exceptions, etc. to specific files
 
 
-  public:
+    /// @brief Configuration for HepMC
+    ///
+    /// Contains macro definitions for printing debug output, feature deprecation, etc.
+    /// Static class - configuration is shared among all HepMC events
+    /// and program threads
+    ///
+    /// @todo Change to a namespace
+    class Setup {
 
-      /// @name Accessors
-      //@{
-
-      /// Get error messages printing flag
-      static bool print_errors()                { return m_is_printing_errors;    }
-      /// set error messages printing flag
-      static void set_print_errors(bool flag)   { m_is_printing_errors   = flag;  }
-
-      /// Get warning messages printing flag
-      static bool print_warnings()              { return m_is_printing_warnings;  }
-      /// Set warning messages printing flag
-      static void set_print_warnings(bool flag) { m_is_printing_warnings = flag;  }
-
-      /// Get debug level
-      static int  debug_level()                 { return m_debug_level;           }
-      /// Set debug level
-      static void set_debug_level(int level)    { m_debug_level          = level; }
-      //@}
-
-      /// @name Static constants
-      //@{
-      /// Default maxUlps for AlmostEqual2sComplement function (double precision)
-      static const unsigned int DEFAULT_DOUBLE_ALMOST_EQUAL_MAXULPS;
-
-      /// Default threshold for comparing double variables
-      static const double DOUBLE_EPSILON;
-
-      //@}
+        /// Private constructor
+        Setup() {}
+        /// Private destructor
+        ~Setup() {}
 
 
-  private:
+    public:
 
-      static bool m_is_printing_errors;   //!< Flag for printing error messages
-      static bool m_is_printing_warnings; //!< Flag for printing warning messages
-      static int  m_debug_level;          //!< Level of debug messages printed out
-  };
+        /// @name Accessors
+        //@{
 
+        /// Get error messages printing flag
+        static bool print_errors()                { return m_is_printing_errors;    }
+        /// set error messages printing flag
+        static void set_print_errors(bool flag)   { m_is_printing_errors   = flag;  }
 
-  /// @name Printing macros
-  //@{
+        /// Get warning messages printing flag
+        static bool print_warnings()              { return m_is_printing_warnings;  }
+        /// Set warning messages printing flag
+        static void set_print_warnings(bool flag) { m_is_printing_warnings = flag;  }
 
-  /** @brief Macro for printing error messages */
-  #define ERROR(MESSAGE)       if( Setup::print_errors() )         { std::cerr << "ERROR::"                 << MESSAGE << std::endl; }
+        /// Get debug level
+        static int  debug_level()                 { return m_debug_level;           }
+        /// Set debug level
+        static void set_debug_level(int level)    { m_debug_level          = level; }
+        //@}
 
-  /** @brief Macro for printing warning messages */
-  #define WARNING(MESSAGE)     if( Setup::print_warnings() )       { std::cout << "WARNING::"               << MESSAGE << std::endl; }
+        /// @name Static constants
+        //@{
+        /// Default maxUlps for AlmostEqual2sComplement function (double precision)
+        static const unsigned int DEFAULT_DOUBLE_ALMOST_EQUAL_MAXULPS;
 
-  // Debug messages and code that will not go to the release version
-  #ifndef HEPMC_RELEASE_VERSION
+        /// Default threshold for comparing double variables
+        static const double DOUBLE_EPSILON;
 
-  /** @brief Macro for printing debug messages with appropriate debug level */
-  #define DEBUG(LEVEL,MESSAGE) if( Setup::debug_level()>=(LEVEL) ) { std::cout << "DEBUG(" << LEVEL <<")::" << MESSAGE << std::endl; }
-
-  /** @brief Macro for storing code useful for debugging */
-  #define DEBUG_CODE_BLOCK( x ) x
-
-  #else
-  #define DEBUG( x,y )
-  #define DEBUG_CODE_BLOCK( x )
-  #endif
-
-  //@}
+        //@}
 
 
-  // Deprecation macro
-  #if __GNUC__ && __cplusplus && HEPMC_NO_DEPRECATION_WARNINGS == 0
-  #define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
-  #if GCC_VERSION >= 40500
-    #define HEPMC_DEPRECATED(x) __attribute__((deprecated(x)))
-  #else
-    #define HEPMC_DEPRECATED(x) __attribute__((deprecated))
-  #endif
-  #else
-    #define HEPMC_DEPRECATED(x)
-  #endif
+    private:
+
+        static bool m_is_printing_errors;   //!< Flag for printing error messages
+        static bool m_is_printing_warnings; //!< Flag for printing warning messages
+        static int  m_debug_level;          //!< Level of debug messages printed out
+    };
+
+
+
+
+    /// @name Printing macros
+    //@{
+
+    /** @brief Macro for printing error messages */
+    #define ERROR(MESSAGE)       if( Setup::print_errors() )         { std::cerr << "ERROR::"                 << MESSAGE << std::endl; }
+
+    /** @brief Macro for printing warning messages */
+    #define WARNING(MESSAGE)     if( Setup::print_warnings() )       { std::cout << "WARNING::"               << MESSAGE << std::endl; }
+
+    // Debug messages and code that will not go to the release version
+    #ifndef HEPMC_RELEASE_VERSION
+
+    /** @brief Macro for printing debug messages with appropriate debug level */
+    #define DEBUG(LEVEL,MESSAGE) if( Setup::debug_level()>=(LEVEL) ) { std::cout << "DEBUG(" << LEVEL <<")::" << MESSAGE << std::endl; }
+
+    /** @brief Macro for storing code useful for debugging */
+    #define DEBUG_CODE_BLOCK( x ) x
+
+    #else
+    #define DEBUG( x,y )
+    #define DEBUG_CODE_BLOCK( x )
+    #endif
+
+    //@}
+
+
+    /// @name Exceptions
+    /// @todo Move to a dedicated header once we have more than one
+    //@{
+
+    /// @todo Should it be GenException? It's a HepMC thing, but not really a "gen" thing
+    struct Exception : public std::runtime_error {
+      Exception(const std::string& msg) : std::runtime_error(msg) {}
+    };
+
+    //@}
+
+
+    /// Deprecation macro
+    #if __GNUC__ && __cplusplus && HEPMC_NO_DEPRECATION_WARNINGS == 0
+    #define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+    #if GCC_VERSION >= 40500
+      #define HEPMC_DEPRECATED(x) __attribute__((deprecated(x)))
+    #else
+      #define HEPMC_DEPRECATED(x) __attribute__((deprecated))
+    #endif
+    #else
+      #define HEPMC_DEPRECATED(x)
+    #endif
+
 
 } // namespace HepMC
 
