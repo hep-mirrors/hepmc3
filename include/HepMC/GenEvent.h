672e13536ff911e96ba59449d6277302800f387d
Tomasz Przedzinski <tprzedzi@cern.ch>
2015-03-24 18:03:19 +0100

    Comment style changed

--- include/HepMC/GenEvent.h
+++ include/HepMC/GenEvent.h
@@ -3,10 +3,10 @@
 // This file is part of HepMC
 // Copyright (C) 2014 The HepMC collaboration (see AUTHORS for details)
 //
-/**
- *  @file GenEvent.h
- *  @brief Definition of \b class GenEvent
- */
+///
+/// @file GenEvent.h
+/// @brief Definition of \b class GenEvent
+///
 #ifndef  HEPMC_GENEVENT_H
 #define  HEPMC_GENEVENT_H
 
@@ -49,43 +49,40 @@ struct GenEventData;
 class GenEvent {
 public:
 
-    /// Default constructor
+    /// @brief Default constructor
     GenEvent(Units::MomentumUnit momentum_unit = Units::GEV, Units::LengthUnit length_unit = Units::MM);
 
     /// @name Content allocation
     //@{
 
-    /** @brief Add particle */
+    /// @brief Add particle
     void add_particle( const GenParticlePtr &p );
 
-    /** @brief Add vertex */
+    /// @brief Add vertex
     void add_vertex( const GenVertexPtr &v );
 
-    /** @brief Add whole tree in topological order
-     *
-     *  This function will find the beam particles (particles
-     *  that have no production vertices or their production vertices
-     *  have no particles) and will add the whole decay tree starting from
-     *  these particles.
-     *
-     *  @note Any particles on this list that do not belong to the tree
-     *        will be ignored.
-     */
+    /// @brief Add whole tree in topological order
+    ///
+    /// This function will find the beam particles (particles
+    /// that have no production vertices or their production vertices
+    /// have no particles) and will add the whole decay tree starting from
+    /// these particles.
+    ///
+    /// @note Any particles on this list that do not belong to the tree
+    ///       will be ignored.
     void add_tree( const vector<GenParticlePtr> &particles );
 
-    /** @brief Reserve memory for particles and vertices
-     *
-     *  Helps optimize event creation when size of the event is known beforehand
-     */
+    /// @brief Reserve memory for particles and vertices
+    ///
+    /// Helps optimize event creation when size of the event is known beforehand
     void reserve(unsigned int particles, unsigned int vertices = 0);
 
-    /** @brief Change event units
-     *
-     *  Converts event from current units to new ones
-     */
+    /// @brief Change event units
+    ///
+    /// Converts event from current units to new ones
     void set_units( Units::MomentumUnit new_momentum_unit, Units::LengthUnit new_length_unit);
 
-    /** @brief Remove contents of this event */
+    /// @brief Remove contents of this event
     void clear();
 
     //@}
@@ -94,63 +91,65 @@ public:
     /// @name Accessors
     //@{
 
-    /// Get event number
+    /// @brief Get event number
     int  event_number() const { return m_event_number; }
-    /// Set event number
+    /// @brief Set event number
     void set_event_number(int num) { m_event_number = num; }
 
-    /// Get/set list of particles
+    /// @brief Get/set list of particles
     const vector<GenParticlePtr>& particles() const { return m_particles; }
-    /// Get/set list of vertices
+    /// @brief Get/set list of vertices
     const vector<GenVertexPtr>& vertices() const { return m_vertices; }
 
-    /// Get momentum unit
+    /// @brief Get momentum unit
     const Units::MomentumUnit& momentum_unit() const { return m_momentum_unit; }
-    /// Get length unit
+    /// @brief Get length unit
     const Units::LengthUnit& length_unit() const { return m_length_unit; }
 
-    /// Get heavy ion generator additional information
+    /// @brief Get heavy ion generator additional information
     const GenHeavyIonPtr heavy_ion() const { return attribute<GenHeavyIon>("GenHeavyIon"); }
-    /// Set heavy ion generator additional information
+    /// @brief Set heavy ion generator additional information
     void set_heavy_ion(const GenHeavyIonPtr &hi) { add_attribute("GenHeavyIon",hi); }
 
-    /// Get PDF information
+    /// @brief Get PDF information
     const GenPdfInfoPtr pdf_info() const { return attribute<GenPdfInfo>("GenPdfInfo"); }
-    /// Set PDF information
+    /// @brief Set PDF information
     void set_pdf_info(const GenPdfInfoPtr &pi) { add_attribute("GenPdfInfo",pi); }
 
-    /// Get cross-section information
+    /// @brief Get cross-section information
     const GenCrossSectionPtr cross_section() const { return attribute<GenCrossSection>("GenCrossSection"); }
-    /// Set cross-section information
+    /// @brief Set cross-section information
     void set_cross_section(const GenCrossSectionPtr &cs) { add_attribute("GenCrossSection",cs); }
 
-    /// Test to see if we have two valid beam particles
+    /// @brief Test to see if we have two valid beam particles
     bool valid_beam_particles() const { return (bool)m_beam_particle_1 && (bool)m_beam_particle_2; }
-    /// Get incoming beam particles
+
+    /// @brief Get incoming beam particles
     pair<GenParticlePtr,GenParticlePtr> beam_particles() const { return make_pair(m_beam_particle_1,m_beam_particle_2); }
-    /// Set incoming beam particles
+
+    /// @brief Set incoming beam particles
     void set_beam_particles(const GenParticlePtr& p1, const GenParticlePtr& p2) { m_beam_particle_1 = p1; m_beam_particle_2 = p2; }
-    /// Set incoming beam particles
+
+    /// @brief Set incoming beam particles
     void set_beam_particles(const pair<GenParticlePtr,GenParticlePtr>& p) { m_beam_particle_1 = p.first; m_beam_particle_2 = p.second; }
 
-    /** @brief Add event attribute to event
-     *
-     *  This will overwrite existing attribute if an attribute
-     *  with the same name is present
-     */
+    /// @brief Add event attribute to event
+    ///
+    /// This will overwrite existing attribute if an attribute
+    /// with the same name is present
     void add_attribute(const string &name, const shared_ptr<Attribute> &att,
 		       int id = 0) {
       if ( att ) m_attributes[name][id] = att;
     }
 
-    /// Remove attribute
+    /// @brief Remove attribute
     void remove_attribute(const string &name, int id = 0);
 
-    /// Get attribute of type T
+    /// @brief Get attribute of type T
     template<class T>
     shared_ptr<T> attribute(const string &name, int id = 0) const;
 
-    /// Get list of attributes
+    /// @brief Get list of attributes
     const map< string, map<int, shared_ptr<Attribute> > >& attributes() const { return m_attributes; }
 
     //@}
@@ -161,27 +160,27 @@ public:
 
     #ifndef HEPMC_NO_DEPRECATED
 
-    /// Add particle by raw pointer
+    /// @brief Add particle by raw pointer
     /// @deprecated Use GenEvent::add_particle( const GenParticlePtr& ) instead
     HEPMC_DEPRECATED("Use GenParticlePtr instead of GenParticle*")
     void add_particle( GenParticle *p );
 
-    /// Add vertex by raw pointer
+    /// @brief Add vertex by raw pointer
     /// @deprecated Use GenEvent::add_vertex( const GenVertexPtr& ) instead
     HEPMC_DEPRECATED("Use GenVertexPtr instead of GenVertex*")
     void add_vertex  ( GenVertex *v );
 
-    /// Set heavy ion generator additional information by raw pointer
+    /// @brief Set heavy ion generator additional information by raw pointer
     /// @deprecated Use GenEvent::set_heavy_ion( GenHeavyIonPtr hi) instead
     HEPMC_DEPRECATED("Use GenHeavyIonPtr instead of GenHeavyIon*")
     void set_heavy_ion(GenHeavyIon *hi);
 
-    /// Set PDF information by raw pointer
+    /// @brief Set PDF information by raw pointer
     /// @deprecated Use GenEvent::set_pdf_info( GenPdfInfoPtr pi) instead
     HEPMC_DEPRECATED("Use GenPdfInfoPtr instead of GenPdfInfo*")
     void set_pdf_info(GenPdfInfo *pi);
 
-    /// Set cross-section information by raw pointer
+    /// @brief Set cross-section information by raw pointer
     /// @deprecated Use GenEvent::set_cross_section( GenCrossSectionPtr cs) instead
     HEPMC_DEPRECATED("Use GenCrossSectionPtr instead of GenCrossSection*")
     void set_cross_section(GenCrossSection *cs);
@@ -245,21 +244,24 @@ public:
     bool vertices_empty()  const { return m_vertices.empty();  }
 
     #endif
-  
+
     //@}
 
   // methods to fill GenEventData and to read it back
 
+  /// @brief Fill GenEventData object
   void write_data(GenEventData &data) const;
+
+  /// @brief Fill GenEvent based on GenEventData
   void read_data(const GenEventData &data);
 
-  
 #ifdef HEPMC_ROOTIO
 
+  /// @brief Root I/O streamer
   void Streamer(TBuffer &b);
 
 #endif
-  
+
 private:
 
     /// @name Fields
@@ -272,10 +274,9 @@ private:
     std::vector<GenParticlePtr> m_particles;       //!< List of particles
     std::vector<GenVertexPtr>   m_vertices;        //!< List of vertices
 
-    /** @brief Map of event, particle and vertex attributes
-     *
-     *  Keys are name and id (0 = event, <0 = vertex, >0 = particle)
-     */
+    /// @brief Map of event, particle and vertex attributes
+    ///
+    /// Keys are name and id (0 = event, <0 = vertex, >0 = particle)
     mutable map< string, map<int, shared_ptr<Attribute> > > m_attributes;
     //@}
 
