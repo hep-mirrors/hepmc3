8f742df2f290829652f193ddc69d2ff17b3ae23c
Andrii Verbytskyi <andriish@hermes.mpp.mpg.de>
2019-06-24 21:40:07 +0200

    Revert "Some fixes after compilation with -Wall -Wextra"
    This reverts commit 5d4e77d355538933fee99a41800bc3734d2f6199.

--- examples/ConvertExample/src/WriterDOT.cc
+++ examples/ConvertExample/src/WriterDOT.cc
@@ -3,10 +3,10 @@ namespace HepMC3
 {
 WriterDOT::WriterDOT(const std::string &filename,shared_ptr<GenRunInfo> run): m_file(filename),
     m_stream(&m_file),
-    m_style(0),
     m_buffer(nullptr),
     m_cursor(nullptr),
-    m_buffer_size( 256*1024 )
+    m_buffer_size( 256*1024 ),
+    m_style(0)
 {
     if ( !m_file.is_open() ) {
         ERROR( "WriterDOT: could not open output file: "<<filename )
@@ -16,10 +16,10 @@ WriterDOT::WriterDOT(const std::string &filename,shared_ptr<GenRunInfo> run): m_
 WriterDOT::WriterDOT(std::ostream &stream, shared_ptr<GenRunInfo> run)
     : m_file(),
       m_stream(&stream),
-      m_style(0),
       m_buffer(nullptr),
       m_cursor(nullptr),
-      m_buffer_size( 256*1024 )
+      m_buffer_size( 256*1024 ),
+      m_style(0)
 {}
 
 
