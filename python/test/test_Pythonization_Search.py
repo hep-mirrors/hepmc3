004caa9409ee820ccab1d3de2b998d23870c4e39
Andrii Verbytskyi <averbyts@cern.ch>
2020-05-09 01:21:28 +0200

    Changed The Pythonization_Search test

--- python/test/test_Pythonization_Search.py
+++ python/test/test_Pythonization_Search.py
@@ -3,12 +3,30 @@ import sys,os
 sys.path=update_path()
 
 from  pyHepMC3TestUtils import COMPARE_ASCII_FILES
+from pyHepMC3 import HepMC3 as hm
 from pyHepMC3.search import HepMC3 as hmsearch
 import random,math
 
 
 def test_Pythonization_Search():
     print (dir(hmsearch))
+    inputA=hm.ReaderAsciiHepMC2("inputPythonization_Search.hepmc")
+    if inputA.failed(): 
+        print("No input")
+        sys.exit(1)
+    while  not inputA.failed():
+        evt=hm.GenEvent()
+        inputA.read_event(evt)
+        if inputA.failed(): 
+            print("End of file reached. Exit.\n")
+            break
+        v=evt.vertices()[2]
+#        va=hmsearch.ancestor_vertices(v)
+#        pa=hmsearch.ancestor_particles(v)
+#        print(v," has vertex   ancestors ",va)
+#        print(v," has particle ancestors ",pa)
+        evt.clear()    
+    inputA.close()
     return 0
 
 if __name__ == "__main__":
