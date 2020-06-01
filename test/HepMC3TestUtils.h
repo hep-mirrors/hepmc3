b8ed067d199816999e42b2c0c9d41e4362e507bb
Andrii Verbytskyi <andrii.verbytskyi@cern.ch>
2020-06-01 18:38:23 +0000

    Add functions to in-memory conversion of the HePMC2 to HePMC3 and vice verse

--- test/HepMC3TestUtils.h
+++ test/HepMC3TestUtils.h
@@ -3,25 +3,38 @@
 #include <stdio.h>
 #include <string.h>
 
-using namespace std;
-
-int COMPARE_ASCII_FILES(const string& f1,const string& f2)
+int skip(const std::string& s1)
 {
-    fstream file1(f1.c_str()), file2(f2.c_str());
-    char string1[16*256], string2[16*256];
-    int j;
-    j = 0;
-    puts("Run comparison");
+    if (s1.length()==0) return 1;
+    if (s1.find("HepMC::Version")!=std::string::npos) return 1;
+    return 0;
+}
+int COMPARE_ASCII_FILES(const std::string& f1,const std::string& f2)
+{
+    std::fstream file1(f1.c_str()), file2(f2.c_str());
+    std::string string1, string2;
+    int j1,j2;
+    j1 = 0;
+    j2 = 0;
+    std::cout <<"Run comparison"<< "\n";
     while((!file1.eof())&&(!file2.eof()))
     {
-        file1.getline(string1,16*256);
-        file2.getline(string2,16*256);
-        j++;
-        if(strcmp(string1,string2) != 0)
+        for (;;) {
+            j1++;
+            if (!std::getline(file1,string1)) break;
+            if (skip(string1)==0) break;
+        }
+        for (;;) {
+            j2++;
+            if (!std::getline(file2,string2)) break;
+            if (skip(string2)==0) break;
+        }
+
+        if(string1.compare(string2) != 0)
         {
-            cout << j << "-th strings are not equal" << "\n";
-            cout << "   " << string1 << "\n";
-            cout << "   " << string2 << "\n";
+            std::cout << j1<<"/"<<j2 << "-th strings are not equal" << f1<<f2<<"\n";
+            std::cout << "   ->" << string1 << "<-\n";
+            std::cout << "   ->" << string2 << "<-\n";
             return 1;
         }
     }
