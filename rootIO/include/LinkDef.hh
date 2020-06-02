f8f0addf0b31d7b81315ec872df6c712a5e27740
Andrii Verbytskyi <andrii.verbytskyi@cern.ch>
2020-06-02 22:52:50 +0000

    Fixed ReaderRootTree compatibility with HepMC3.0

--- rootIO/include/LinkDef.hh
+++ rootIO/include/LinkDef.hh
@@ -13,16 +13,15 @@
 #pragma link C++ class HepMC3::Setup+;
 
 /* To generate dictionaries for compatibility with HepMC3.0*/
-#pragma link C++ struct HepMC::GenEventData+;
-#pragma link C++ struct HepMC::GenRunInfoData+;
-#pragma link C++ struct HepMC::GenParticleData+;
-#pragma link C++ struct HepMC::GenVertexData+;
-#pragma link C++ class std::vector<HepMC::GenParticleData>+;
-#pragma link C++ class std::vector<HepMC::GenVertexData>+;
-#pragma link C++ class HepMC::FourVector+;
-#pragma link C++ class HepMC::Units+;
-#pragma link C++ class HepMC::Setup+;
-
+#pragma link C++ typedef HepMC::GenEventData+;
+#pragma link C++ typedef HepMC::GenRunInfoData+;
+#pragma link C++ typedef HepMC::GenParticleData+;
+#pragma link C++ typedef HepMC::GenVertexData+;
+#pragma link C++ typedef std::vector<HepMC::GenParticleData>+;
+#pragma link C++ typedef std::vector<HepMC::GenVertexData>+;
+#pragma link C++ typedef HepMC::FourVector+;
+#pragma link C++ typedef HepMC::Units+;
+#pragma link C++ typedef HepMC::Setup+;
 
 
 #endif
