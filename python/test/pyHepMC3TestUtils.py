46233b233b9bc2cda60fad5bf5ec38c8fe42f9aa
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2020-06-19 17:11:11 +0200

    Python formatted with black. Update python versions

--- python/test/pyHepMC3TestUtils.py
+++ python/test/pyHepMC3TestUtils.py
@@ -1,33 +1,35 @@
 import re
-import sys,os
+import sys, os
+
 
 def python_label():
-    v=sys.version_info
-    return str(v[0])+"."+str(v[1])+"."+str(v[2])
-  
+    v = sys.version_info
+    return str(v[0]) + "." + str(v[1]) + "." + str(v[2])
+
+
 def update_path():
- return [os.path.abspath(os.path.join(os.pardir,python_label()))]+[os.getcwd()]+sys.path
+    return [os.path.abspath(os.path.join(os.pardir, python_label()))] + [os.getcwd()] + sys.path
 
 
-def COMPARE_ASCII_FILES(f1,f2):
- file1=open(f1)
- file2=open(f2)
- print ("Run comparison")
- string1=" "
- string2=" "
- j=0
- while string1 and string2:
-     j=j+1
-     string1=file1.readline()
-     string2=file2.readline()
-     if (string1 != string2):
-            print (j,"-th strings are not equal", "\n")
-            print ("   ", string1 ,"\n")
-            print ("   ", string2 ,"\n")
-            if not re.match(r'HepMC::Version',string1):
-             file1.close()
-             file2.close()
-             return 1
- file1.close()
- file2.close()
- return 0
+def COMPARE_ASCII_FILES(f1, f2):
+    file1 = open(f1)
+    file2 = open(f2)
+    print("Run comparison")
+    string1 = " "
+    string2 = " "
+    j = 0
+    while string1 and string2:
+        j = j + 1
+        string1 = file1.readline()
+        string2 = file2.readline()
+        if string1 != string2:
+            print(j, "-th strings are not equal", "\n")
+            print("   ", string1, "\n")
+            print("   ", string2, "\n")
+            if not re.match(r"HepMC::Version", string1):
+                file1.close()
+                file2.close()
+                return 1
+    file1.close()
+    file2.close()
+    return 0
--- python/test/test_Pythia8.py
+++ python/test/test_Pythia8.py
@@ -1,10 +1,13 @@
-from  pyHepMC3TestUtils import update_path,python_label
+from pyHepMC3TestUtils import update_path, python_label
 import sys
-sys.path=update_path()
+
+sys.path = update_path()
 
 from pyHepMC3 import HepMC3 as hm
 import pythia8 as p8
 from Pythia8ToHepMC3 import Pythia8ToHepMC3
+
+
 def test_Pythia8():
     try:
         pythia = p8.Pythia()
@@ -15,24 +18,26 @@ def test_Pythia8():
     pythia.readString("Beams:idB = -11")
     pythia.readString("Beams:eCM =  91.2")
     pythia.init()
-    p8tohm=Pythia8ToHepMC3 ()
-    p8tohm.m_store_pdf=True
-    out=hm.WriterAscii(python_label()+"test_Pythia.hepmc")
+    p8tohm = Pythia8ToHepMC3()
+    p8tohm.m_store_pdf = True
+    out = hm.WriterAscii(python_label() + "test_Pythia.hepmc")
     for iEvent in range(0, 100):
-        if not pythia.next(): continue
+        if not pythia.next():
+            continue
         nCharged = 0
-        evt=hm.GenEvent()
-        p8tohm.fill_next_event1(pythia,evt,iEvent)
+        evt = hm.GenEvent()
+        p8tohm.fill_next_event1(pythia, evt, iEvent)
         out.write_event(evt)
         evt.clear()
     pythia.stat()
     out.close()
     return 0
 
+
 if __name__ == "__main__":
-    result=1
+    result = 1
     try:
-        result=test_Pythia8()
+        result = test_Pythia8()
     except:
-        result=1
+        result = 1
     sys.exit(result)
