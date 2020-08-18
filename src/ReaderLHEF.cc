36bdf878dfaa09e068976ee367a3648462e78817
Andrii Verbytskyi <averbyts@cern.ch>
2020-08-18 11:23:18 +0200

    Better reader for LHEF

--- src/ReaderLHEF.cc
+++ src/ReaderLHEF.cc
@@ -82,6 +82,12 @@ ReaderLHEF::~ReaderLHEF() {close();};
 
 bool ReaderLHEF::read_event(GenEvent& ev)
 {
+    if (m_storage.size()>0)
+    {
+        ev=m_storage.front();
+        m_storage.pop_front();
+        return m_failed;
+    }
     m_failed=!(m_reader->readEvent());
     if (m_failed) return m_failed;
     // To each GenEvent we want to add an attribute corresponding to
@@ -91,45 +97,58 @@ bool ReaderLHEF::read_event(GenEvent& ev)
     std::shared_ptr<HEPEUPAttribute> hepe = std::make_shared<HEPEUPAttribute>();
     if ( m_reader->outsideBlock.length() )
         hepe->tags =  LHEF::XMLTag::findXMLTags(m_reader->outsideBlock);
+
     hepe->hepeup = m_reader->hepeup;
-    ev.set_event_number(m_neve);
-    m_neve++;
-    // This is just a text to check that we can add additional
-    // attributes to each event.
-    ev.add_attribute("HEPEUP", hepe);
-    ev.add_attribute("AlphaQCD",
-                     std::make_shared<DoubleAttribute>(hepe->hepeup.AQCDUP));
-    ev.add_attribute("AlphaEM",
-                     std::make_shared<DoubleAttribute>(hepe->hepeup.AQEDUP));
-    ev.add_attribute("NUP",
-                     std::make_shared<IntAttribute>(hepe->hepeup.NUP));
-    ev.add_attribute("IDPRUP",
-                     std::make_shared<LongAttribute>(hepe->hepeup.IDPRUP));
-
-    // Now add the Particles from the LHE event to HepMC
-    std::vector<GenParticlePtr> particles;
-    std::map< std::pair<int,int>, GenVertexPtr> vertices;
-    for ( int i = 0; i < hepe->hepeup.NUP; ++i )
+    printf("%i\n",hepe->hepeup.subevents.size());
+    std::vector<LHEF::HEPEUP*> input;
+    if (m_reader->hepeup.subevents.size()>0) input.insert(input.end(),hepe->hepeup.subevents.begin(),hepe->hepeup.subevents.end());
+    else (input.push_back(&m_reader->hepeup));
+    int first_group_event=m_neve;
+    for (auto ahepeup: input)
     {
-        particles.push_back(std::make_shared<GenParticle>(hepe->momentum(i),hepe->hepeup.IDUP[i],hepe->hepeup.ISTUP[i]));
-        if (i<2) continue;
-        std::pair<int,int> vertex_index(hepe->hepeup.MOTHUP[i].first,hepe->hepeup.MOTHUP[i].second);
+        GenEvent evt;
+        evt.set_event_number(m_neve);
+        m_neve++;
+        evt.add_attribute("AlphaQCD", std::make_shared<DoubleAttribute>(ahepeup->AQCDUP));
+        evt.add_attribute("AlphaEM", std::make_shared<DoubleAttribute>(ahepeup->AQEDUP));
+        evt.add_attribute("NUP",  std::make_shared<IntAttribute>(ahepeup->NUP));
+        evt.add_attribute("IDPRUP",std::make_shared<LongAttribute>(ahepeup->IDPRUP));
+        if (hepe->hepeup.subevents.size()) evt.add_attribute("eventgroup", std::make_shared<LongAttribute>(first_group_event));
+        // Now add the Particles from the LHE event to HepMC
+        std::vector<GenParticlePtr> particles;
+        std::map< std::pair<int,int>, GenVertexPtr> vertices;
+        for ( int i = 0; i < ahepeup->NUP; ++i )
+        {
+            FourVector mom((ahepeup->PUP)[i][0],(ahepeup->PUP)[i][1],(ahepeup->PUP)[i][2],(ahepeup->PUP)[i][3]);
+            particles.push_back(std::make_shared<GenParticle>(mom,ahepeup->IDUP[i],ahepeup->ISTUP[i]));
+            if (i<2) continue;
+            std::pair<int,int> vertex_index(ahepeup->MOTHUP[i].first,ahepeup->MOTHUP[i].second);
+            if (vertices.find(vertex_index)==vertices.end())vertices[vertex_index]=std::make_shared<GenVertex>();
+            vertices[vertex_index]->add_particle_out(particles.back());
+        }
+        for ( auto v: vertices )
+        {
+            std::pair<int,int> vertex_index=v.first;
+            GenVertexPtr          vertex=v.second;
+            for (int i=vertex_index.first-1; i<vertex_index.second; i++) if (i>=0&&i<particles.size()) vertex->add_particle_in(particles[i]);
+        }
+        std::pair<int,int> vertex_index(0,0);
         if (vertices.find(vertex_index)==vertices.end())vertices[vertex_index]=std::make_shared<GenVertex>();
-        vertices[vertex_index]->add_particle_out(particles.back());
-    }
-    for ( auto v: vertices )
-    {
-        std::pair<int,int> vertex_index=v.first;
-        GenVertexPtr          vertex=v.second;
-        for (int i=vertex_index.first-1; i<vertex_index.second; i++) if (i>=0&&i<particles.size()) vertex->add_particle_in(particles[i]);
-    }
-    for ( auto v: vertices ) ev.add_vertex(v.second);
+        for (size_t i=0; i<particles.size(); i++) if (!particles[i]->end_vertex()&&!particles[i]->production_vertex())
+                if (i<2) vertices[vertex_index]->add_particle_in(particles[i]);
+                else vertices[vertex_index]->add_particle_out(particles[i]);
 
-    // And we also want to add the weights.
-    std::vector<double> wts;
-    for ( int i = 0, N = hepe->hepeup.weights.size(); i < N; ++i )
-        wts.push_back(hepe->hepeup.weights[i].first);
-    ev.weights() = wts;
+        for ( auto v: vertices ) evt.add_vertex(v.second);
+
+        // And we also want to add the weights.
+        std::vector<double> wts;
+        for ( int i = 0, N = ahepeup->weights.size(); i < N; ++i )
+            wts.push_back(ahepeup->weights[i].first);
+        evt.weights() = wts;
+        m_storage.push_back(evt);
+    }
+    ev=m_storage.front();
+    m_storage.pop_front();
     return m_failed;
 }
 /// @brief Return status of the stream
