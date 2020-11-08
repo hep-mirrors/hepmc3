7205d467da4254eeec623dd534f9da8a5c6e7317
Andrii Verbytskyi <averbyts@cern.ch>
2020-11-08 15:24:32 +0100

    Update the docs and prepare to 3.2.3

--- include/HepMC3/ReaderAscii.h
+++ include/HepMC3/ReaderAscii.h
@@ -1,7 +1,7 @@
 // -*- C++ -*-
 //
 // This file is part of HepMC
-// Copyright (C) 2014-2019 The HepMC collaboration (see AUTHORS for details)
+// Copyright (C) 2014-2020 The HepMC collaboration (see AUTHORS for details)
 //
 #ifndef HEPMC3_READERASCII_H
 #define HEPMC3_READERASCII_H
--- include/HepMC3/Version.h
+++ include/HepMC3/Version.h
@@ -1,7 +1,7 @@
 // -*- C++ -*-
 //
 // This file is part of HepMC
-// Copyright (C) 2014-2019 The HepMC collaboration (see AUTHORS for details)
+// Copyright (C) 2014-2020 The HepMC collaboration (see AUTHORS for details)
 //
 #ifndef HEPMC3_VERSION_H
 #define HEPMC3_VERSION_H
@@ -9,12 +9,12 @@
 #include <string>
 
 /// HepMC version string
-#define HEPMC3_VERSION "3.02.02"
+#define HEPMC3_VERSION "3.02.03"
 
 /// @brief HepMC version as an integer, HepMC X.Y.Z = 1000000*X + 1000*Y + Z
 ///
 /// Use like "#if HEPMC3_VERSION_CODE < 3001004" for < 3.01.04
-#define HEPMC3_VERSION_CODE 3002002
+#define HEPMC3_VERSION_CODE 3002003
 namespace HepMC3 {
 /// Get the HepMC library version string
 inline std::string version() {
