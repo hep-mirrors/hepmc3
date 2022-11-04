8229be38ed675e011dec7cdee27a465d86180f6d
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2022-11-04 21:25:38 +0100

    SOme more pythonization

--- python/src/__init__.py
+++ python/src/__init__.py
@@ -10,23 +10,32 @@ def Pythonic_Writer_write(self,evt):
    self.write_event(evt)
 
 
-setattr(HepMC3.Writer, "write", Pythonic_Writer_write)
-setattr(HepMC3.Reader, "read",  Pythonic_Reader_read)
+setattr(HepMC3.Writer, "py_write", Pythonic_Writer_write)
+setattr(HepMC3.Reader, "py_read",  Pythonic_Reader_read)
 
-setattr(HepMC3.GenEvent, "Xrun_info", property(lambda self: self.run_info(),lambda self,x: self.set_run_info(x)))
-setattr(HepMC3.GenEvent, "Xevent_number", property(lambda self: self.event_number(),lambda self,x: self.set_event_number(x)))
-setattr(HepMC3.GenEvent, "Xlength_unit", property(lambda self: self.length_unit(),lambda self,x: self.set_units(self.momentum_unit(),x)))
-setattr(HepMC3.GenEvent, "Xmomentum_unit", property(lambda self: self.momentum_unit(),lambda self,x: self.set_units(x,self.length_unit())))
-setattr(HepMC3.GenEvent, "Xparticles", property(lambda self: list(self.particles())))
-setattr(HepMC3.GenEvent, "Xvertices", property(lambda self: list(self.vertices())))
-
-
-
-setattr(HepMC3.GenRunInfo, "Xtools", property(lambda self: self.tools()))
-setattr(HepMC3.GenRunInfo, "Xweight_names", property(lambda self: list(self.weight_names())))
+#setattr(HepMC3.GenEvent, "py_weights", property(lambda self: list(self.weights()),lambda self,x: self.set_weights(x)))
+setattr(HepMC3.GenEvent, "py_run_info", property(lambda self: self.run_info(),lambda self,x: self.set_run_info(x)))
+setattr(HepMC3.GenEvent, "py_event_number", property(lambda self: self.event_number(),lambda self,x: self.set_event_number(x)))
+setattr(HepMC3.GenEvent, "py_length_unit", property(lambda self: self.length_unit(),lambda self,x: self.set_units(self.momentum_unit(),x)))
+setattr(HepMC3.GenEvent, "py_momentum_unit", property(lambda self: self.momentum_unit(),lambda self,x: self.set_units(x,self.length_unit())))
+setattr(HepMC3.GenEvent, "py_particles", property(lambda self: list(self.particles())))
+setattr(HepMC3.GenEvent, "py_vertices", property(lambda self: list(self.vertices())))
 
 
 
-setattr(HepMC3.Setup, "Xprint_warnings", property(lambda self: self.print_warnings,lambda self,x: self.set_print_warnings(x)))
-setattr(HepMC3.Setup, "Xprint_errors", property(lambda self: self.print_errors,lambda self,x: self.set_print_errors(x)))
-setattr(HepMC3.Setup, "Xdebug_level", property(lambda self: self.debug_level,lambda self,x: self.set_debug_level(x)))
+setattr(HepMC3.GenRunInfo, "py_tools", property(lambda self: self.tools()))
+setattr(HepMC3.GenRunInfo, "py_weight_names", property(lambda self: list(self.weight_names())))
+
+
+setattr(HepMC3.GenParticle, "py_generated_mass", property(lambda self: self.generated_mass(),lambda self,x: self.set_generated_mass(x)))
+setattr(HepMC3.GenParticle, "py_pid", property(lambda self: self.pid(),lambda self,x: self.set_pid(x)))
+setattr(HepMC3.GenParticle, "py_status", property(lambda self: self.status(),lambda self,x: self.set_status(x)))
+setattr(HepMC3.GenParticle, "py_momentum", property(lambda self: self.momentum(),lambda self,x: self.set_momentum(x)))
+setattr(HepMC3.GenParticle, "py_parents", property(lambda self: list(self.production_vertex().particles_in())))
+
+#setattr(HepMC3.GenHeavyIon, "py_status", property(lambda self: self.Ncoll_hard(),lambda self,x: self.set_status(x)))
+
+
+setattr(HepMC3.Setup, "py_print_warnings", property(lambda self: self.print_warnings,lambda self,x: self.set_print_warnings(x)))
+setattr(HepMC3.Setup, "py_print_errors", property(lambda self: self.print_errors,lambda self,x: self.set_print_errors(x)))
+setattr(HepMC3.Setup, "py_debug_level", property(lambda self: self.debug_level,lambda self,x: self.set_debug_level(x)))
