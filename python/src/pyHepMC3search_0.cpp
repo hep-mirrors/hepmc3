888821942fa8480be046c2b9698168bf9604eccc
Andrii Verbytskyi <andrii.verbytskyi@cern.ch>
2023-09-13 13:20:16 +0200

    Python CMake  simplification

--- python/src/pyHepMC3search_0.cpp
+++ python/src/pyHepMC3search_0.cpp
@@ -14,7 +14,7 @@
 #include <HepMC3/Version.h>
 #include <HepMC3/Relatives.h>
 #include <HepMC3/Selector.h>
-#include <src/search_binders.h>
+#include <src/searchbinders.h>
 
 
 #ifndef BINDER_PYBIND11_TYPE_CASTER
@@ -60,5 +60,12 @@ void bind_pyHepMC3search_0(std::function< pybind11::module &(std::string const &
 		cl.def("assign", (class HepMC3::Selector & (HepMC3::Selector::*)(const class HepMC3::Selector &)) &HepMC3::Selector::operator=, "C++: HepMC3::Selector::operator=(const class HepMC3::Selector &) --> class HepMC3::Selector &", pybind11::return_value_policy::automatic, pybind11::arg(""));
 
 		binder::custom_Selector_binder(cl);
+	}
+	// HepMC3::abs(const class HepMC3::Selector &) file:HepMC3/Selector.h line:161
+	M("HepMC3").def("abs", (class std::shared_ptr<const class HepMC3::Selector> (*)(const class HepMC3::Selector &)) &HepMC3::abs, "ConstSelectorPtr abs\n\nC++: HepMC3::abs(const class HepMC3::Selector &) --> class std::shared_ptr<const class HepMC3::Selector>", pybind11::arg("input"));
+
+	{ // HepMC3::StandardSelector file:HepMC3/Selector.h line:174
+		pybind11::class_<HepMC3::StandardSelector, std::shared_ptr<HepMC3::StandardSelector>, HepMC3::Selector> cl(M("HepMC3"), "StandardSelector", "StandardSelector ");
+		cl.def("assign", (class HepMC3::StandardSelector & (HepMC3::StandardSelector::*)(const class HepMC3::StandardSelector &)) &HepMC3::StandardSelector::operator=, "C++: HepMC3::StandardSelector::operator=(const class HepMC3::StandardSelector &) --> class HepMC3::StandardSelector &", pybind11::return_value_policy::automatic, pybind11::arg(""));
 	}
 }
