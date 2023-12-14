c28bf5a5957f3a0c4d7a2716fa75e635f9442bc8
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2023-12-14 22:12:43 +0100

    added different versions

--- examples/PruneExample/prune.h
+++ examples/PruneExample/prune.h
@@ -7,7 +7,7 @@
 #include <HepMC3/WriterAscii.h>
 #include <list>
 namespace HepMC3 {
-
+// Assuming vector starage and allowing momenta noncoservation 
 template <typename Select>
 void remove_particles1(GenEvent& event, Select select) {
     auto& particles = event.particles();
@@ -33,6 +33,7 @@ void remove_particles1(GenEvent& event, Select select) {
     for (auto& vtx : vert_toremove) event.remove_vertex(vtx);
 }
 
+// Assuming list starage and allowing momenta noncoservation
 template <typename Select>
 void remove_particles2(GenEvent& event, Select select) {
     auto& particles = event.particles();
@@ -49,6 +50,57 @@ void remove_particles2(GenEvent& event, Select select) {
         if (prdVtx) prdVtx->remove_particle_out(particle);
         if (endVtx && prdVtx && prdVtx != endVtx && endVtx->particles_in_size() == 0) {
           for (auto& outgoing : endVtx->particles_out()) prdVtx->add_particle_out(outgoing);
+        }
+    }
+    for (auto& particle : part_toremove) event.remove_particle(particle);
+    for (auto& vtx : event.vertices()) if (vtx->particles_out().empty() && vtx->particles_in().empty()) vert_toremove.push_back(vtx);
+    for (auto& vtx : vert_toremove) event.remove_vertex(vtx);
+}
+// Assuming vector starage
+template <typename Select>
+void remove_particles3(GenEvent& event, Select select) {
+    auto& particles = event.particles();
+    std::vector<GenParticlePtr> part_toremove;
+    part_toremove.reserve(particles.size());
+    std::vector<GenVertexPtr> vert_toremove;
+    vert_toremove.reserve(event.vertices_size());
+    for (auto& particle : particles) {
+        if (select(particle)) continue;
+        part_toremove.push_back(particle);
+        auto endVtx = particle->end_vertex();
+        auto prdVtx = particle->production_vertex();
+        if (endVtx) endVtx->remove_particle_in(particle);
+        if (prdVtx) prdVtx->remove_particle_out(particle);
+        if (endVtx && prdVtx && prdVtx != endVtx ) {
+          for (auto& outgoing : endVtx->particles_out()) prdVtx->add_particle_out(outgoing);
+          for (auto& incoming : endVtx->particles_in()) prdVtx->add_particle_in(incoming);
+        }
+    }
+    std::sort(part_toremove.begin(),part_toremove.end(), [](const auto& a, consta auto& b) {return a->id()<b->id();});
+    for (auto& particle : part_toremove) event.remove_particle(particle);
+    for (auto& vtx : event.vertices()) if (vtx->particles_out().empty() && vtx->particles_in().empty()) vert_toremove.push_back(vtx);
+    std::sort(vert_toremove.begin(),vert_toremove.end(), [](const auto& a, consta auto& b) {return a->id()>b->id();});
+    for (auto& vtx : vert_toremove) event.remove_vertex(vtx);
+}
+
+// Assuming list starage
+template <typename Select>
+void remove_particles4(GenEvent& event, Select select) {
+    auto& particles = event.particles();
+    std::vector<GenParticlePtr> part_toremove;
+    part_toremove.reserve(particles.size());
+    std::vector<GenVertexPtr> vert_toremove;
+    vert_toremove.reserve(event.vertices_size());
+    for (auto& particle : particles) {
+        if (select(particle)) continue;
+        part_toremove.push_back(particle);
+        auto endVtx = particle->end_vertex();
+        auto prdVtx = particle->production_vertex();
+        if (endVtx) endVtx->remove_particle_in(particle);
+        if (prdVtx) prdVtx->remove_particle_out(particle);
+        if (endVtx && prdVtx && prdVtx != endVtx ) {
+          for (auto& outgoing : endVtx->particles_out()) prdVtx->add_particle_out(outgoing);
+          for (auto& incoming : endVtx->particles_in()) prdVtx->add_particle_in(incoming);
         }
     }
     for (auto& particle : part_toremove) event.remove_particle(particle);
